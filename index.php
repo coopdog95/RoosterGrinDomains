4d9a345cc22de07388029a2fc3e3068bedff6338
Cooper Timmerman <coopert925@gmail.com>
2020-05-10 16:31:05 -0700

    styles still not working

--- index.php
+++ index.php
@@ -12,7 +12,8 @@ $response = getDomains();
   <head>
     <title>Domains | Rooster Grin</title>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="./styles.css">
+    <style><?php include 'styles.css'; ?></style>
+    <!-- <link rel="stylesheet" href="./styles.css"> -->
     <link rel="shortcut icon" href="https://www.roostergrin.com/wp-content/themes/cockerel_s-child/favicon.ico">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
