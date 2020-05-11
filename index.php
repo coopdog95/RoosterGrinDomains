6f980cbe72dbfa65faf6f4e167688467b7fe7f5d
Cooper Timmerman <coopert925@gmail.com>
2020-05-10 17:44:06 -0700

    script stuff

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ $response = getDomains();
 
   </head>
   <body>
-    <!-- <pre><?php //print_r($response); ?></pre> -->
+    <pre><?php print_r($response); ?></pre>
     <div class="heading container">
       <h1>Domains</h1>
       <h3><?php echo count($domains); ?> domains available!</h3>
--- searchScript.js
+++ searchScript.js
@@ -2,5 +2,12 @@ var domains = [];
 for (var i = 0; i < response.length; i++) {
   let domain = response[i]["domain"]
   domains += domain
-  console.log(domain)
+  // console.log(domain)
+}
+
+function findMatches(wordToMatch, d) {
+  return d.filter(domain => {
+    const regex = new RegExp(wordToMatch, 'gi');
+    return d.match(regex);
+  })
 }
