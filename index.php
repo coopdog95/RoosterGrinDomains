cefecbd9862a27ec6275398057bba88b6c6b01ad
Cooper Timmerman <coopert925@gmail.com>
2020-05-20 20:03:29 -0700

    yupp

--- index.php
+++ index.php
@@ -39,7 +39,7 @@ $response = getDomains();
     </div>
     <div class="search-container container">
       <!-- Search form -->
-        <input class="search-bar" type="text" placeholder="Search..." aria-label="Search">
+        <input class="search-bar" id="searchy" type="text" placeholder="Search..." aria-label="Search">
 
     </div>
 
