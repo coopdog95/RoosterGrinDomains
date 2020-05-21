d9583599bc458974b6448d2d47fb2c7fc524390d
Cooper Timmerman <coopert925@gmail.com>
2020-05-20 18:34:00 -0700

    fun typo

--- searchScript.js
+++ searchScript.js
@@ -9,7 +9,7 @@ for (var i = 0; i < response.length; i++) {
 
 function findMatches(wordToMatch, domains) {
   const wordsArray = wordToMatch.split(" ");
-  console.log('wordArray('+wordArray.length+'): ' + wordsArray);
+  console.log('wordsArray('+wordsArray.length+'): ' + wordsArray);
   var retArray = [];
   wordsArray.forEach((word, i) => {
     var a = domains.filter(domain => {
