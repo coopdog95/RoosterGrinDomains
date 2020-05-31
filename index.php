e021814b9fd5e713df6931527c549d916d5203f6
Cooper Timmerman <coopert925@gmail.com>
2020-05-30 17:23:35 -0700

    asdasdfsf

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $fetchedDomains = main();
 <html lang="en" dir="ltr">
   <head>
     <title>Domains | Rooster Grin</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0">
     <meta charset="utf-8">
     <style><?php include 'styles.css'; ?></style>
     <!-- <link rel="stylesheet" href="./styles.css"> -->
